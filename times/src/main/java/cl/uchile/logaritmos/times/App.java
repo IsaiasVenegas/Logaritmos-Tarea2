7ef937962018bd79b903e8ed1ac90d7ecde58c41
Isaias Venegas <ivenegas@reveal.co>
2023-06-04 04:28:04 -0400

    Import files from previous repo

---
+++ times/src/main/java/cl/uchile/logaritmos/times/App.java
@@ -0,0 +1,108 @@
+package cl.uchile.logaritmos.times;
+
+
+import cl.uchile.logaritmos.rbt.RedBlackTree;
+import cl.uchile.logaritmos.st.SplayTree;
+
+/**
+ * Tarea 2
+ * Implementacion de los arboles de busqueda binaria Splay Tree y Red-Black Tree
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        float[] alphas = {0.0f, 0.5f, 1.0f, 1.5f}; // equiprobables, 50% de exito, 100% de exito, 150% de exito
+        int expN = 15; //  n=2^16 == 1<<15
+        while(expN <= 23){ // n=2^24 == 1<<23
+            // Creamos un arreglo pi con n elementos
+            int n = 1 << expN;
+            int[] pi = Utils.createN(n);
+
+            // Insertamos los elementos de pi en un Splay Tree
+            SplayTree<Integer> splayTree = new SplayTree<>();
+            for (int i = 0; i < n; i++) {
+                splayTree.insert(pi[i]);
+            }
+
+            // Insertar los elementos de pi en un Red-Black Tree
+            RedBlackTree<Integer> redBlackTree = new RedBlackTree<>();
+            for (int i = 0; i < n; i++) {
+                redBlackTree.insert(pi[i]);
+            }
+
+            for(int a=0; a<=3; a++){
+                long totalTimeST = 0;
+                long[] partialTimesST = new long[5];
+
+                long totalTimeRBT = 0;
+                long[] partialTimesRBT = new long[5];
+
+                for(int reps = 1; reps<=5; reps++){
+                    // Creamos un arreglo c con m elementos (m=2^28) con elementos repetidos segun alpha
+                    int[] c = Utils.createC(pi, n, alphas[a]);
+
+                    // Buscamos los elementos de c en el Splay Tree y medimos su tiempo
+                    long startTime = System.currentTimeMillis();
+
+                    for (int i = 0; i < c.length; i++) {
+                        if(c[i]!=0){ // Omitimos los 0s
+                            splayTree.find(c[i]);
+                        }
+
+                    }
+
+                    long endTime = System.currentTimeMillis();
+                    long parcialTime = endTime - startTime;
+                    totalTimeST += parcialTime;
+                    partialTimesST[reps-1] = parcialTime;
+
+                    System.out.println("expN= "+ (expN+1) +" | alpha= "+ alphas[a]+ " | Tiempo de busqueda en Splay Tree: " + parcialTime + " ms");
+
+                    // Buscamos los elementos de c en el Red-Black Tree y medimos su tiempo
+                    startTime = System.currentTimeMillis();
+
+                    for (int i = 0; i < c.length; i++) {
+                        if(c[i]!=0){ // Omitimos los 0s
+                            redBlackTree.find(c[i]);
+                        }
+                    }
+
+                    endTime = System.currentTimeMillis();
+                    parcialTime = endTime - startTime;
+                    totalTimeRBT += parcialTime;
+                    partialTimesRBT[reps-1] = parcialTime;
+
+                    System.out.println("expN= "+ (expN+1) +" | alpha= "+ alphas[a]+ " | Tiempo de busqueda en Red-Black Tree: " + parcialTime + " ms");
+                }
+                System.out.println("====================================================================================================");
+
+                long averageTimeST = totalTimeST/5;
+                long varianceST = 0;
+                for(int i=0; i<5; i++){
+                    varianceST += Math.pow(partialTimesST[i]-averageTimeST, 2);
+                }
+                varianceST /= 5;
+                long standardDeviationST = (long) Math.sqrt(varianceST);
+                System.out.println("expN= "+ (expN+1) +" | alpha= "+ alphas[a]+ " | Tiempo promedio de busqueda en Splay Tree: " + averageTimeST + " ms");
+                System.out.println("expN= "+ (expN+1) +" | alpha= "+ alphas[a]+ " | Varianza en tiempo de busqueda en Splay Tree: " + varianceST + " ms^2");
+                System.out.println("expN= "+ (expN+1) +" | alpha= "+ alphas[a]+ " | Desviacion estandar de busqueda en Splay Tree: " + standardDeviationST + " ms");
+
+                long averageTimeRBT = totalTimeRBT/5;
+                long varianceRBT = 0;
+                for(int i=0; i<5; i++){
+                    varianceRBT += Math.pow(partialTimesRBT[i]-averageTimeRBT, 2);
+                }
+                varianceRBT /= 5;
+                long standardDeviationRBT = (long) Math.sqrt(varianceRBT);
+                System.out.println("expN= "+ (expN+1) +" | alpha= "+ alphas[a]+ " | Tiempo promedio de busqueda en Red-Black Tree: " + averageTimeRBT + " ms");
+                System.out.println("expN= "+ (expN+1) +" | alpha= "+ alphas[a]+ " | Varianza en tiempo de busqueda en Red-Black Tree: " + varianceRBT + " ms^2");
+                System.out.println("expN= "+ (expN+1) +" | alpha= "+ alphas[a]+ " | Desviacion estandar de busqueda en Red-Black Tree: " + standardDeviationRBT + " ms");
+
+                System.out.println("====================================================================================================");
+            }
+
+            expN++;
+        }
+    }
+}
---
+++ times/src/main/java/cl/uchile/logaritmos/times/Utils.java
@@ -0,0 +1,88 @@
+package cl.uchile.logaritmos.times;
+
+import java.util.Random;
+
+public class Utils {
+    // Función que crea una permutacion aleatoria pi dado n
+    public static int[] createN(int n) {
+        // Creamos el espacio en memoria para el arreglo
+        int[] pi = new int[n];
+
+        // Asignamos valores ordenados al arreglo
+        for (int i = 0; i < n; i++) {
+            pi[i] = i + 1;
+        }
+
+        // Desordenamos el arreglo
+        Random rand = new Random();
+        for (int i = n - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            int temp = pi[i];
+            pi[i] = pi[j];
+            pi[j] = temp;
+        }
+
+        return pi;
+    }
+
+    // Funcion que crea una secuencia de busqueda con elementos alpha-probables
+    public static int[] createC(int[] pi, int n, float alpha) {
+        int expM = 27; // m es siempre 2^28
+        int m = 1 << expM;
+        int[] c = new int[m];
+
+        float epsilon = 0.0000001f;
+        if (Math.abs(alpha) < epsilon) {
+            // Caso Equiprobable
+            int expN = 0;
+            // Calculamos el exponente de N
+            while (1 << expN < n) {
+                expN++;
+            }
+
+            // Creamos phi que indica la cantidad de repeticiones de cada elemento i en pi
+            // Y lo guardamos en C
+            float phiDecimal = (1 << expM) >> expN;
+            int phiInteger = (int) Math.ceil(phiDecimal);
+            int currentIndex = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < phiInteger; j++) {
+                    c[currentIndex] = pi[i];
+                    currentIndex++;
+                }
+            }
+
+        } else {
+            // Primero calculamos el denominador de phi(i)
+            double sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += Math.floor(Math.pow(i + 1, alpha));
+            }
+
+            // Creamos phi que indica la cantidad de repeticiones de cada elemento i en pi
+            // Y lo guardamos en C
+            int currentIndex = 0;
+            for (int i = 0; i < n; i++) {
+                double fi = Math.floor(Math.pow(i + 1, alpha));
+                int phi = (int) Math.floor((m * fi) / sum);
+                for (int j = 0; j < phi; j++) {
+                    c[currentIndex] = pi[i];
+                    currentIndex++;
+                }
+            }
+        }
+
+        // Desordenamos el arreglo para finalmente retornarlo
+        Random rand = new Random();
+        for (int i = m - 1; i > 0; i--) {
+            int k = rand.nextInt(i + 1);
+            int temp = c[i];
+            c[i] = c[k];
+            c[k] = temp;
+        }
+
+        return c;
+    }
+
+
+}
